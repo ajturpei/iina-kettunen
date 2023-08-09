d5c03964271aa5e5ac45ca89119379259ecf02f8
Aleksi Turpeinen <aleksi.turpeinen@sok.fi>
2023-08-09 12:19:50 +0300

    add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,15 @@
-import {Divider} from 'src/components/UI/generalLayoutStyles'
+import Image from 'next/image'
+import {
+  Divider,
+  Ingress,
+  Paragraph,
+  TwoColumns,
+} from 'src/components/UI/generalLayoutStyles'
+import profilePic from '../../../public/iina_portrait.jpg'
+import {IMG} from 'src/components/Header/HeaderStyles'
 
 export const metadata = {
-  title: 'About | Iina Kettunent',
+  title: 'About | Iina Kettunen',
 }
 
 const AboutPage = () => {
@@ -9,6 +17,27 @@ const AboutPage = () => {
     <>
       <Divider />
       <h1>Helsinki based furniture and set designer</h1>
+      <TwoColumns>
+        <div>
+          <Ingress>
+            Iina Kettunen is a Helsinki-based product and furniture designer.
+            Her passion lies in well-thought aesthetics of everyday life.
+          </Ingress>
+          <Paragraph>
+            Iina Kettunen is a Helsinki-based product and furniture designer.
+            Her passion lies in well-thought aesthetics of everyday life. With
+            an uncompromising eye for detail and knowledge for quality, Iina's
+            designs serve a purpose in a poetic manner.
+          </Paragraph>
+          <Paragraph>
+            Iina Kettunen utilizes her profound knowledge of material and
+            proportion also in set design.
+          </Paragraph>
+        </div>
+        <div>
+          <IMG alt="Iina Kettunen portrait" src="/iina_portrait.jpg" />
+        </div>
+      </TwoColumns>
     </>
   )
 }
--- src/components/UI/generalLayoutStyles.ts
+++ src/components/UI/generalLayoutStyles.ts
@@ -1,5 +1,6 @@
 'use client'
 
+import {device} from 'src/theme/breakpoints'
 import styled from 'styled-components'
 
 export const MainContainer = styled.main`
@@ -11,4 +12,26 @@ export const Divider = styled.div`
   height: 1px;
   width: 100%;
   padding-top: ${({theme}) => theme.spacing.xmedium};
+`
+
+export const Ingress = styled.h3`
+  font-size: ${({theme}) => theme.spacing.xmedium};
+  line-height: 1.25;
+  margin-bottom: 2rem;
+  @media ${device.tablet} {
+    font-size: ${({theme}) => theme.spacing.slarge};
+    margin-bottom: 4rem;
+  }
+`
+export const Paragraph = styled.p`
+  margin-bottom: 1rem;
+`
+
+export const TwoColumns = styled.div`
+  display: grid;
+  gap: 4rem;
+  grid-template-columns: 1fr;
+  @media ${device.tablet} {
+    grid-template-columns: 1fr 1fr;
+  }
 `
